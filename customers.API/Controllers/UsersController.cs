5ea3baf7518b79cdc2dc67671af7479d95800841
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-30 02:33:20 -0600

    Fail test expecting to get an Ok() status code 200

--- customers.API/Controllers/UsersController.cs
+++ customers.API/Controllers/UsersController.cs
@@ -6,12 +6,17 @@ namespace customers.API.Controllers;
 [Route("[controller]")]
 public class UsersController : ControllerBase
 {
-    private readonly ILogger<UsersController> _logger;
+    // private readonly ILogger<UsersController> _logger;
 
-    public UsersController(ILogger<UsersController> logger)
+    public UsersController()
     {
-        _logger = logger;
+
     }
+
+    // public UsersController(ILogger<UsersController> logger)
+    // {
+    //     _logger = logger;
+    // }
 
     [HttpGet(Name = "GetUsers")]
     public async Task<IActionResult> Get()
--- customers.UnitTests/SystemsTests/Controllers/TestUsersController.cs
+++ customers.UnitTests/SystemsTests/Controllers/TestUsersController.cs
@@ -1,10 +1,17 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+using customers.API.Controllers;
+
 namespace customers.UnitTests.SystemsTests.Controllers;
 
 public class TestUsersController
 {
     [Fact]
-    public void Test1()
+    public async Task GetStatus200()
     {
-
+        var users_controller_instance = new UsersController();
+        var response = (OkObjectResult)await users_controller_instance.Get();
+        response.StatusCode.Should().Be(200);
     }
 }
