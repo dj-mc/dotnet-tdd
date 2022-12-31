a0316a8b71f8b7e7573fa5485e2f3fbf75709197
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-31 15:46:51 -0600

    Test GetUsers service invocation occurs once

--- customers.UnitTests/SystemsTests/Controllers/TestUsersController.cs
+++ customers.UnitTests/SystemsTests/Controllers/TestUsersController.cs
@@ -1,7 +1,9 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 
 using customers.API.Controllers;
+using customers.API.Models;
 
 namespace customers.UnitTests.SystemsTests.Controllers;
 
@@ -10,8 +12,37 @@ public class TestUsersController
     [Fact]
     public async Task GetStatus200()
     {
-        var users_controller_instance = new UsersController();
+        // Resolve instance of implemented interface
+        var mock_users_service = new Mock<IUsersService>();
+
+        // Setup mocked IUsersService type,
+        // returning a List of Users.
+        mock_users_service
+            .Setup(service => service.GetUsers())
+            .ReturnsAsync(new List<User>());
+
+        // Provide service as dependency of UsersController,
+        // and access mocked implementation via dot Object.
+        var users_controller_instance = new UsersController(mock_users_service.Object);
+
         var response = (OkObjectResult)await users_controller_instance.Get();
         response.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task InvokeService()
+    {
+        var mock_users_service = new Mock<IUsersService>();
+
+        mock_users_service
+            .Setup(service => service.GetUsers())
+            .ReturnsAsync(new List<User>());
+
+        var users_controller_instance = new UsersController(mock_users_service.Object);
+        var response = await users_controller_instance.Get();
+
+        // Verify invocation of service.GetUsers() occurs once
+        mock_users_service.
+            Verify(service => service.GetUsers(), Times.Once());
     }
 }
