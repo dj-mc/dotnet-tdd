99917e5a0174a1092dc61097969b0317768ace9c
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-31 15:45:14 -0600

    Invoke GetUsers service as controller's Get method

--- customers.API/Controllers/UsersController.cs
+++ customers.API/Controllers/UsersController.cs
@@ -2,25 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace customers.API.Controllers;
 
+// Configure controller to serve HTTP API responses
 [ApiController]
 [Route("[controller]")]
+// Base class for MVC controller (sans view support)
 public class UsersController : ControllerBase
 {
-    // private readonly ILogger<UsersController> _logger;
+    private readonly IUsersService _users_service;
 
-    public UsersController()
+    // Create UsersController with its interface as a dependency
+    public UsersController(IUsersService users_service)
     {
-
+        _users_service = users_service;
     }
-
-    // public UsersController(ILogger<UsersController> logger)
-    // {
-    //     _logger = logger;
-    // }
 
     [HttpGet(Name = "GetUsers")]
     public async Task<IActionResult> Get()
     {
-        return null;
+        var retrieved_users = await _users_service.GetUsers();
+        // OkObjectResult object, producing status code 200
+        return Ok("Everything is Good");
     }
 }
