0fff58b5e0e647e481e7e10f7b823e34c337080e
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-31 15:12:56 -0600

    User and Address models

---
+++ customers.API/Models/Address.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Address
+{
+    public string City { get; set; }
+    public string ZipCode { get; set; }
+    public string Street { get; set; }
+}
---
+++ customers.API/Models/User.cs
@@ -0,0 +1,8 @@
+namespace customers.API.Models;
+
+public class User {
+    public int ID { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public Address Address { get; set; }
+}
